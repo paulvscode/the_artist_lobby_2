6799a8254db147dacc2c00f9e611147624444e99
paulvscode <67148083+paulvscode@users.noreply.github.com>
2021-11-24 17:07:56 +0100

    added admin links

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -7,6 +7,8 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Project;
+use App\Entity\User;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -28,5 +30,8 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linktoRoute('Back to the website', 'fas fa-home', 'project_index');
+        yield MenuItem::linkToCrud('Projects', 'fas fa-map-marker-alt', Project::class);
+        yield MenuItem::linkToCrud('Users', 'fas fa-comments', User::class);
     }
 }
